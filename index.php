fa514dc3adffe783319df254372448bd1b0f57a8
gusbui <47858725+gusbui@users.noreply.github.com>
2019-12-17 19:28:30 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
     echo "PHP"
+	$dbc=mysqli_connect('gusbuiserveris.database.windows.net','gusbui', 'Passw0rd');
+	if(!$dbc){
+		die ("Negaliu prisijungti prie MySQL:"	.mysqli_error($dbc));
+	}
+	else {
+		echo "CONNECTION SUCCESS";
+	}
 ?>
 <html>
 <body>
